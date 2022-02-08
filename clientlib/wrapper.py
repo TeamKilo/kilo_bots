826dbb4615cac7d340a0d8b3865d6064cbbfdb3b
MilkFansHelloWorld <31027215+MilkFansHelloWorld@users.noreply.github.com>
2022-02-08 13:03:22 +0000

    Wrote the client side wrapper

--- clientlib/wrapper.py
+++ clientlib/wrapper.py
@@ -5,6 +5,21 @@ BASE_URL = os.environ[
     'GAME_SERVER_BASE_URL'] if 'GAME_SERVER_BASE_URL' in os.environ else "https://team-kilo-server.herokuapp.com"
 
 
+class ClientLibBaseException(Exception):
+    pass
+
+
+class ClientLibRequestException(ClientLibBaseException):
+    def __init__(self, message, status=None):
+        self.status = status
+        super(ClientLibRequestException, self).__init__(message)
+
+
+class ClientLibInternalException(ClientLibBaseException):
+    def __init__(self, message):
+        super(ClientLibInternalException, self).__init__(message)
+
+
 class GenericGameState:
     def update_game_state(self, encoded_game_state: dict):
         """
@@ -23,26 +38,86 @@ class GenericGameMove:
         pass
 
 
-class GenericGameClient():
-    def __init__(self, game_type):
+class GenericGameClient:
+    def __init__(self, game_type, game_id=None, username=None):
         self.game_type = game_type
-        self.username = ""
-        self.game_id = ""
+        self.username = username
+        self.game_id = game_id
         self.game_state = None
         self.session_id = ""
 
     # Create game
     def create_game(self):
-        pass
+        res = requests.post("{}/api/create-game".format(BASE_URL), json={"name": self.game_type})
+        if res.ok:
+            res_json = res.json()
+            if res_json['game_id'] != "":
+                self.game_id = res_json['game_id']
+            else:
+                raise ClientLibRequestException("No Game Id Returned")
+        else:
+            raise ClientLibRequestException(res.reason, res.status_code)
 
-    def join_game(self, username: str):
-        pass
+    def join_game(self, game_id: str = None, username: str = None):
+        if username is not None:
+            self.username = username
+        if game_id is not None:
+            self.game_id = game_id
+        res = requests.post("{}/api/join-game".format(BASE_URL),
+                            json={"game_id": self.game_id, "username": self.username})
+        if res.ok:
+            res_json = res.json()
+            if res_json['session_id'] != "":
+                self.session_id = res_json['session_id']
+            else:
+                raise ClientLibRequestException("No Session Id Returned")
+        else:
+            raise ClientLibRequestException(res.reason, res.status_code)
 
-    def get_state_and_update(self, current_state: GenericGameState) -> dict:
-        pass
+    def get_state(self) -> dict:
+        """ get state and return it as a dictionary """
+        if self.game_id is None:
+            raise ClientLibInternalException("No Game Id provided, you need to call create game or construct a client "
+                                             "with a game id")
+        res = requests.get("{}/api/{}/get-state".format(BASE_URL, self.game_id))
+        if res.ok:
+            res_json = res.json()
+            return res_json
+        else:
+            raise ClientLibRequestException(res.reason, res.status_code)
+
+    def update_state(self, current_state: GenericGameState) -> ():
+        """ get state and update the state object """
+        current_state.update_game_state(self.get_state())
 
     def submit_move(self, move: GenericGameMove):
-        pass
+        if self.game_id is None:
+            raise ClientLibInternalException("No Game Id provided, you need to call create game or construct a client "
+                                             "with a game id")
+        if self.session_id is None:
+            raise ClientLibInternalException("No Session Id provided. Please call join_game to join a game first.")
+        res = requests.post("{}/api/{}/submit-move".format(BASE_URL, self.game_id),
+                            json={"session_id": self.session_id, "payload": move.encode_game_state()})
+        if res.ok:
+            res_json = res.json()
+            if not res_json["updated"]:
+                raise ClientLibRequestException("Update failed")
+            else:
+                return
+        else:
+            raise ClientLibRequestException(res.reason, res.status_code)
 
     def wait_for_update(self) -> bool:
-        pass
+        """ Blocks and waits for server to respond.
+        Returns true when SOME update has happened, false if timeout (5 seconds)"""
+        try:
+            res = requests.get("{}/api/{}/wait-for-update".format(BASE_URL, self.game_id), timeout=5)
+        except requests.exceptions.Timeout as e:
+            return False
+        if res is not None:
+            if res.ok:
+                res_json = res.json()
+                return res_json["updated"]
+            else:
+                raise ClientLibRequestException(res.reason, res.status_code)
+        return False
--- main.py
+++ main.py
@@ -1,2 +1,37 @@
+import clientlib.wrapper as wrapper
+
+
+class Connect4State(wrapper.GenericGameState):
+    players = []
+    state = "waiting"
+    can_move = []
+    winners = []
+    game_name = "connect_4"
+    game_state = {"cells": [], "winner": ""}
+
+    def update_game_state(self, encoded_game_state: dict):
+        self.players = encoded_game_state['players']
+        self.state = encoded_game_state['state']
+        self.can_move = encoded_game_state['can_move']
+        self.winners = encoded_game_state['winners']
+        self.game_state = encoded_game_state['game_state']
+
+    def get_players(self):
+        return self.players
+
+    def get_state(self):
+        return self.state
+
+    def is_waiting_for_start(self):
+        return self.state == "wait"
+
+
+class Connect4Move(wrapper.GenericGameMove):
+    def encode_game_state(self):
+        pass
+
+
+
 if __name__ == '__main__':
     print('Hello, World!')
+
